dae718e5f4adca547ae87b28061727a048112f15
刘利 <li.liu@jrj.com.cn>
2017-07-19 14:42:54 +0800

    427 v2 回溯

--- 427_generate-parentheses/generate-parentheses.java
+++ 427_generate-parentheses/generate-parentheses.java
@@ -3,25 +3,33 @@ public class Solution {
      * @param n n pairs
      * @return All combinations of well-formed parentheses
      */
-    public ArrayList<String> generateParenthesis(int n) {
-        // Write your code here
-        ArrayList<String> result = new ArrayList();
-        if (n == 1) {
-            result.add("()");
-            return result;
-        }
-        ArrayList<String> list = generateParenthesis(n - 1);
-        Set<String> set = new HashSet();
-        for (String str : list) {
-            set.add("()" + str);
-            for (int i = 1; i < str.length(); i++) {
-                set.add(str.substring(0, i) + "()" + str.substring(i));
-            }
-            set.add(str + "()");
-        }
-        result.addAll(set);
-        return result;
-    }
+     public ArrayList<String> generateParenthesis(int n) {
+         ArrayList<String> result = new ArrayList<String>();
+         if (n <= 0) {
+             return result;
+         }
+         helper(result, "", n, n);
+         return result;
+     }
+
+ 	public void helper(ArrayList<String> result,
+ 	                   String paren, // current paren
+ 	                   int left,     // how many left paren we need to add
+ 	                   int right) {  // how many right paren we need to add
+ 		if (left == 0 && right == 0) {
+ 			result.add(paren);
+ 			return;
+ 		}
+
+         if (left > 0) {
+ 		    helper(result, paren + "(", left - 1, right);
+         }
+
+         if (right > 0 && left < right) {
+ 		    helper(result, paren + ")", left, right - 1);
+         }
+ 	}
+
 }
 // 给定 n 对括号，请写一个函数以将其生成新的括号组合，并返回所有组合结果。
 // 样例
