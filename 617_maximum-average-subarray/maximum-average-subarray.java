b7ee2e3d172a3e1df83c6f764734246dfb3d0f42
刘利 <li.liu@jrj.com.cn>
2017-07-28 14:56:15 +0800

    617 V2 O(n)时间

--- 617_maximum-average-subarray/maximum-average-subarray.java
+++ 617_maximum-average-subarray/maximum-average-subarray.java
@@ -6,21 +6,38 @@ public class Solution {
      */
     public double maxAverage(int[] nums, int k) {
         // Write your code here
-        double max = Double.MIN_VALUE;
+        double max = Integer.MIN_VALUE;
         int n = nums.length;
-        for (int i = k; i <= n; i++) {
-            long sum = 0;
-            int j = 0;
-            for (j = 0; j < i; j++){
-                sum += nums[j];
+        //先计算前k个数的平均值(构建长度为k的窗口)
+        double sum = 0;
+        int end = 0;
+        while (end < k) {
+            sum += nums[end++];
+        }
+        double avg = sum / k;
+        max = Math.max(max, avg);
+
+        //窗口后部逐渐往后移动，并同时判断窗口前部是否需要移动
+        int start = 0;
+        double sumFront = 0;
+        while (end < n) {
+            //后移一格
+            sum += nums[end++];
+            sumFront += nums[end - 1 - k];
+            //如果窗口前部移动后平均值会变大，说明前部的数字已经拖低了结果，可以移动了
+            if ((sum - sumFront) / k > sum / (end - start)) {
+                sum -= sumFront;
+                sumFront = 0;
+                start = end - k;
             }
-            max = Math.max(max, (double)sum / i);
-            while (j < n) {
-                sum -= nums[j - i];
-                sum += nums[j++];
-                max = Math.max(max, (double)sum / i);
+            avg = sum / (end - start);
+            if (avg > max) {
+                max = Math.max(max, avg);
+                //System.out.println("start : " + start + ", end : " + end + ", avg : " + avg);
             }
         }
+        //System.out.println("start : " + start);
+        //System.out.println("end : " + end);
         return max;
     }
 }
